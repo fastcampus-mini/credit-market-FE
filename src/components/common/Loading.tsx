16ba76f93197d1558c18e97fc86d47972835410a
team7 <hnhyerim@gmail.com>
2023-02-13 00:32:52 +0900

    Feat: 헤더, 로딩, 내브바 UI / UX

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,32 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 const Loading = () => {
-  return <div>로딩중...</div>;
+  return (
+    <LoadingAni>
+      <iframe src="https://embed.lottiefiles.com/animation/97952"></iframe>
+    </LoadingAni>
+  );
 };
 
 export default Loading;
+
+const LoadingAni = styled.div`
+  border: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(1.5px);
+
+  iframe {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    .container:hover .attribution {
+      display: none;
+    }
+  }
+`;
