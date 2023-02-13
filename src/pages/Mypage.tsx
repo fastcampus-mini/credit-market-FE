9b920e16a7431406118450d6fa1cec2daf1c5c02
team7 <hnhyerim@gmail.com>
2023-02-13 21:20:11 +0900

    Feat: 경로에 따른 navbar 이동 설정하기 위한 임시 코드

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,7 +1,27 @@
+import { getMypageItemList } from '@/apis/mypage';
+import { ERROR_MESSAGES } from '@/constants/messages';
+import { hideLoading, showLoading } from '@/store/loadingSlice';
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const Mypage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        dispatch(showLoading());
+        const data = await getMypageItemList();
+      } catch (error) {
+        alert(ERROR_MESSAGES.ERROR_MYPAGE);
+      } finally {
+        dispatch(hideLoading());
+      }
+    }
+    getData();
+  }, []);
+
   return <MypageStyle>Mypage</MypageStyle>;
 };
 
