30b9edee66e08aa3b0eb7676d3ffbf88d502ab17
DavidOH77 <gotjddl7@gmail.com>
2023-02-13 21:04:13 +0900

    Feat: create signin form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router';
+import colors from '@/styles/colors';
+
+const HomeStyle = styled.div`
+  padding: 20px 10px;
+  position: relative;
+  z-index: 1;
+
+  #lottie {
+    width: 100px;
+  }
+`;
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // 엔터키 입력시 서브밋 되는 거 막아야함
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const name = formData.get('name') as string;
+    const email = formData.get('email') as string;
+    console.log({ name, email });
+  };
+
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/', { state: '/login' });
+  };
+  const goSignup = () => {
+    navigate('/signup', { state: '/login' });
+  };
+
+  return (
+    <HomeStyle>
+      <div className="title">Credit Market</div>
+      <p>email</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit" onClick={goHome}>
+          LOGIN
+        </button>
+      </form>
+
+      <button onClick={goSignup}>JOIN</button>
+    </HomeStyle>
+  );
 };
 
 export default SignIn;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
 import { FiArrowLeft } from 'react-icons/fi';
 import { useNavigate } from 'react-router';
+import colors from '@/styles/colors';
+import { isValidateEmail } from '@/utils/validLogic';
 
 const HomeStyle = styled.div`
   padding: 20px 10px;
@@ -16,7 +18,7 @@ const HomeStyle = styled.div`
 const Signup = () => {
   const navigate = useNavigate();
   const goHome = () => {
-    navigate('/home', { state: '/signup' });
+    navigate('/', { state: '/signup' });
   };
 
   const goLogin = () => {
@@ -24,12 +26,15 @@ const Signup = () => {
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // 엔터키 입력시 서브밋 되는 거 막아야함
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     const name = formData.get('name') as string;
     const email = formData.get('email') as string;
     console.log({ name, email });
   };
+
+  const emailRef = useRef<HTMLInputElement>(null);
 
   return (
     <HomeStyle>
@@ -40,9 +45,14 @@ const Signup = () => {
         <h1>credit market</h1>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="이메일" />
-        <input type="text" name="password" placeholder="비밀번호 (8자 이상)" />
-        <input type="text" name="password-check" placeholder="비밀번호 확인" />
+        <input type="text" name="email" placeholder="이메일" ref={emailRef} />
+        {emailRef.current && isValidateEmail(emailRef.current.value) ? ( // useState 이용해야 할듯
+          <p style={{ color: 'green' }}> 확인 완료 </p>
+        ) : (
+          <p style={{ color: 'red' }}> 이메일 에러 </p>
+        )}
+        <input type="password" name="password" placeholder="비밀번호 (8자 이상)" />
+        <input type="password" name="password-check" placeholder="비밀번호 확인" />
         <input type="text" name="name" placeholder="이름" />
         <input type="number" name="birth" placeholder="생년월일" />
         <select name="sex" id="">
@@ -59,7 +69,7 @@ const Signup = () => {
           <option value="job6">직장인</option>
           <option value="job7">학생</option>
         </select>
-        <input type="number" placeholder="개인신용점수" />
+        <input type="number" name="credit" placeholder="개인신용점수" />
         <select name="loan" id="">
           <option value="">선호 대출 종류</option>
           <option value="">중장기 신용 대출</option>
