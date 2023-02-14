885ea770eb690972350776dddb990be151f1cb1f
team7 <hnhyerim@gmail.com>
2023-02-14 14:26:47 +0900

    Style: secondary color #19418A에서 #000540으로 변경 & component 적용

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,9 +1,9 @@
 const COLORS = {
   primary: '#19418A',
-  secondary: '#19418A',
+  secondary: '#000540',
   lightGray: '#dddddd',
   mainText: '#333',
-  loginBackground: '#4831d4',
+  homeBackground: '#4831d4',
   background: '#f9fbff',
   black: '#000',
   white: '#fff',
