34e4d261fc4b51afc89e801e5cf833d0c95382fb
autumnly1007 <autumnly1007@gmail.com>
2023-02-15 16:15:02 +0900

    Feat: 회원가입 / 로그인 화면에서 헤더, 내브바 안보이도록 수정

--- src/interfaces/store.ts
+++ src/interfaces/store.ts
@@ -1,3 +1,4 @@
 export interface IStore {
   loading: Boolean;
+  signPage: Boolean;
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import signPage from './signPageSlice';
 import loading from './loadingSlice';
 
 export default configureStore({
-  reducer: { loading: loading.reducer },
+  reducer: {
+    loading: loading.reducer,
+    signPage: signPage.reducer,
+  },
 });
