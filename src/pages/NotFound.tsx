759aad8cbf82c53f0fc1e371abbda1d249d717c9
autumnly1007 <autumnly1007@gmail.com>
2023-02-19 14:20:04 +0900

    Feat: NotFound 페이지 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,32 @@
+import Button from '@/components/common/Button';
+import { ROUTES } from '@/constants/routes';
+import styled from '@emotion/styled';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <NotFoundContainer>
+      <p>존재하지 않는 페이지입니다.</p>
+      <div>
+        <Button width="200px" onClick={() => navigate(ROUTES.HOME)}>
+          HOME
+        </Button>
+      </div>
+    </NotFoundContainer>
+  );
 };
 
 export default NotFound;
+
+const NotFoundContainer = styled.div`
+  margin-top: 280px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: center;
+  justify-content: center;
+  width: 100%;
+`;
