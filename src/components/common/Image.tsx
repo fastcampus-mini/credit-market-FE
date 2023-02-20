3eb82117adc0aff4afa63d645a07ef6bccbe4267
autumnly1007 <autumnly1007@gmail.com>
2023-02-20 18:07:06 +0900

    Feat: 이미지 컴포넌트 alt prop 추가

--- src/components/common/Image.tsx
+++ src/components/common/Image.tsx
@@ -6,10 +6,13 @@ interface Prop {
   width?: string;
   height?: string;
   borderRadius?: string;
+  alt: string;
 }
 
-const Image = ({ src, width, height, borderRadius }: Prop) => {
-  return <StyledImage src={src} width={width} height={height} borderRadius={borderRadius} />;
+const Image = ({ src, width, height, borderRadius, alt }: Prop) => {
+  return (
+    <StyledImage src={src} width={width} height={height} borderRadius={borderRadius} alt={alt} />
+  );
 };
 
 export default Image;
