722b15204150b1740ea16d3b3b8cbd44a17ed8a2
autumnly1007 <autumnly1007@gmail.com>
2023-02-20 13:32:17 +0900

    Refactor: 상품 카드에 인터페이스 적용

---
+++ src/components/Product/ProductCard.tsx
@@ -0,0 +1,100 @@
+import { ROUTES } from '@/constants/routes';
+import COLORS from '@/styles/colors';
+import styled from '@emotion/styled';
+import React from 'react';
+import Button from '../common/Button';
+import { useNavigate } from 'react-router-dom';
+import Input from '../common/Input';
+import { IProducts } from '@/interfaces/product';
+import { getBankLogo } from '@/utils/bankLogo';
+
+interface Props {
+  data: IProducts;
+}
+
+const ProductCard = ({
+  data: { id, bankName, productName, isFavor, loanType, rateAvg, rateType },
+}: Props) => {
+  const navigate = useNavigate();
+  return (
+    <StyledProductCard>
+      <div className="cardCon">
+        <div className="logoTitle">
+          <img className="bankLogo" src={getBankLogo(bankName!)} alt={bankName} />
+          <h2 className="bankTitle">{bankName}</h2>
+        </div>
+        <p className="productName">{productName}</p>
+        <div className="textBox">
+          <p>
+            대출종류<span>{loanType}</span>
+          </p>
+          <p>
+            평균금리<span>{rateAvg}</span>
+          </p>
+          <p>
+            금리구분<span>{rateType}</span>
+          </p>
+        </div>
+        <Button width="100%" height="50px" onClick={() => navigate(ROUTES.PRODUCT_BY_ID(id))}>
+          자세히 보기
+        </Button>
+      </div>
+      <Input classType="heartBtn" inputType="checkbox" id={id} top="15px" right="35px" />
+    </StyledProductCard>
+  );
+};
+
+export default ProductCard;
+
+const StyledProductCard = styled.li`
+  background-color: ${COLORS.white};
+  border-radius: 10px;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+  padding: 0.6rem;
+  border: 1px solid ${COLORS.lightGray};
+  font-size: 12px;
+  font-weight: bold;
+  position: relative;
+
+  .cardCon {
+    .logoTitle {
+      display: flex;
+      gap: 10px;
+      font-weight: bold;
+      align-items: center;
+      margin-bottom: 10px;
+      color: ${COLORS.primary};
+
+      .bankLogo {
+        width: 30px;
+      }
+    }
+
+    .productName {
+      font-size: 16px;
+      font-weight: bold;
+      margin-bottom: 25px;
+    }
+
+    .textBox {
+      display: flex;
+      gap: 1.6rem;
+      margin-bottom: 20px;
+
+      p {
+        display: flex;
+        width: 25%;
+        flex-direction: column;
+        color: ${COLORS.gray};
+        font-weight: normal;
+
+        span {
+          color: ${COLORS.mainText};
+          font-size: 16px;
+          font-weight: bold;
+          margin-top: 5px;
+        }
+      }
+    }
+  }
+`;
