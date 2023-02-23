a06ea3c4f274bd0631c51f13da6fa73a90c06467
team7 <hnhyerim@gmail.com>
2023-02-23 19:31:45 +0900

    Design:홈 배너 폰트 변경

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -12,14 +12,6 @@ export default GlobalStyle;
 const style = css`
   ${reset}
   @font-face {
-    font-family: 'Binggrae-Two';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Binggrae-Bold.woff')
-      format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
     font-family: 'GmarketSansMedium';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff')
       format('woff');
