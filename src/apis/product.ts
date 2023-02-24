b93d6716fdc94e1785c0a5ffa3323bcdef0fb3a3
DavidOH77 <gotjddl7@gmail.com>
2023-02-24 16:36:35 +0900

    Fix: 타입 오류 수정

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -1,14 +1,14 @@
 import { IProduct } from '@/interfaces/product';
 import { axiosInstance } from './instance';
 import { API_URLS } from '../constants/apiUrls';
-import { SearchFormData } from '../constants/apiUrls';
+import { ISearch } from '@/interfaces/Search';
 
 export const getRecommentList = async () => {
   const data: IProduct[] = await axiosInstance.get(API_URLS.RECOMMEND);
   return data;
 };
 
-export const getSearchList = async (Formdata: SearchFormData) => {
+export const getSearchList = async (Formdata: ISearch) => {
   const data: IProduct[] = await axiosInstance.get(API_URLS.SEARCH(Formdata));
   return data;
 };
