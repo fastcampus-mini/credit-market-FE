b8fb3f32f4add067eb733d7a42f9d022cf2129aa
team7 <hnhyerim@gmail.com>
2023-02-26 18:21:15 +0900

    Feat: MyBuyslice 추가

---
+++ src/store/myBuySlice.ts
@@ -0,0 +1,19 @@
+import { IBuy } from '@/interfaces/buy';
+import { createSlice } from '@reduxjs/toolkit';
+
+const myBuy = createSlice({
+  name: 'myBuy',
+  initialState: [],
+  reducers: {
+    setMyBuyState(state, action) {
+      return (state = action.payload);
+    },
+    deleteMybuyState(state, action) {
+      return (state = state.filter((item: IBuy) => item.productId !== action.payload));
+    },
+  },
+});
+
+export let { setMyBuyState, deleteMybuyState } = myBuy.actions;
+
+export default myBuy;
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,12 +5,14 @@ import modal from './modalSlice';
 import search from './SearchSlice';
 import reduxProducts from './reduxProducts';
 import favor from './favorSlice';
+import myBuy from './myBuySlice';
 
 export const store = configureStore({
   reducer: {
     loading: loading.reducer,
     cart: cart.reducer,
     favor: favor.reducer,
+    myBuy: myBuy.reducer,
     modal: modal.reducer,
     search: search.reducer,
     reduxProducts: reduxProducts.reducer,
