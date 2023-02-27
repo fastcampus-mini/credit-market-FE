1ac193f1224b5c144ab57b23ce4d5c32b483450a
team7 <hnhyerim@gmail.com>
2023-02-27 21:27:36 +0900

    Fix: Minified React error 해결중

--- src/components/template/AvatarIcon.tsx
+++ src/components/template/AvatarIcon.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
-// import Avatar, { genConfig } from 'react-nice-avatar';
+import Avatar, { genConfig } from 'react-nice-avatar';
 
 interface Prop {
   width?: string;
